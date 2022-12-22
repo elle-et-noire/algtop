14572e3b60e0a65b125a23080f20498803bdee0e
Lomega <atibon3@gmail.com>
2022-12-23 07:45:08 +0900

    isomorph

--- theories/group.v
+++ theories/group.v
@@ -18,8 +18,7 @@ Structure Binop `(A : Ensemble X) := {
 }.
 
 Class Associative `{A : Ensemble X} (op : Binop A) := {
-  assoc :
-    forall x y z : A, op x (op y z) == op (op x y) z 
+  assoc : forall x y z : A, op x (op y z) == op (op x y) z 
 }.
 
 Class LIdentical `{A : Ensemble X} (op : Binop A) e := {
@@ -37,7 +36,6 @@ Class Identical `{A : Ensemble X} (op : Binop A) e := {
 #[global]
 Existing Instances id_identl id_identr.
 
-
 Class LInvertible `{A : Ensemble X} (op : Binop A) e (inv : Map A A) := {
   invl : forall x : A, op (inv x) x == e
 }.
@@ -53,13 +51,12 @@ Class Invertible `{A : Ensemble X} (op : Binop A) e (inv : Map A A) := {
 #[global]
 Existing Instances inv_invl inv_invr.
 
-
 Class IsGroup `{supp : Ensemble X} (mul : Binop supp) 
               (inv : Map supp supp) e :=
 {
-  mulgA :> Associative mul;
-  mulg1 :> RIdentical mul e;
-  mulgV :> RInvertible mul e inv
+  assocg :> Associative mul;
+  identrg :> RIdentical mul e;
+  invrg :> RInvertible mul e inv
 }.
 
 Structure Group X := {
@@ -79,9 +76,9 @@ Arguments idg {_} {_}.
 
 Notation "[ A | *: op , !: inv , 1: id ]" :=
   (@Build_Group _ A op inv id _)
-  (at level 0, A, op, inv, id at level 99, no associativity) : group_scope.
+  (at level 0, A, op, inv, id at level 99) : group_scope.
 Notation "[ *: op , !: inv , 1: id ]" := [ _ | *: op, !: inv, 1: id]
-  (at level 0, op, inv, id at level 99, no associativity) : group_scope.
+  (at level 0, op, inv, id at level 99) : group_scope.
 Notation "(  * 'in' G  )" := (@mulg _ G) : group_scope.
 Notation "( * )" := ( * in _ ) : group_scope.
 Notation "g * h 'in' G" := (@mulg _ G g h)
@@ -97,16 +94,157 @@ Notation "! g 'in' G" := (@invg _ G g)
 Notation "! g" := ( ! g in _ )
   (at level 35, right associativity) : group_scope.
 
-Lemma mul1g : forall `{G : Group X}, LIdentical ( * in G ) 1.
+Lemma identlg `{G : Group X} : LIdentical ( * in G ) 1.
 Proof.
-  split. intros x.
-  assert (1 * x == 1 * (x * 1)) as Heq0.
-  { now rewrite identr. }
-  assert (x * 1 == x * !x * !!x) as Heq1.
-  { now rewrite <-(invr (!x)), assoc. }
-  rewrite Heq0, Heq1, invr, assoc, identr.
-  assert (1 == x * !x) as Heq2. { now rewrite invr. }
-  now rewrite Heq2, <-assoc, invr, identr.
+  split. intros x. rewrite <-identr. rewrite <-(invr (!x)) at 2.
+  now rewrite assoc, <-(assoc 1), invr, identr,
+    <-(invr x), <-assoc, invr, identr.
 Qed.
 #[global]
-Existing Instance mul1g.
+Existing Instance identlg.
+
+Lemma invlg `{G : Group X} : LInvertible ( * in G ) 1 ( ! ).
+Proof.
+  split. intros x. rewrite <-identr. rewrite <-(invr (!x)) at 1.
+  now rewrite assoc, <-(assoc _ x), invr, identr, invr.
+Qed.
+#[global]
+Existing Instance invlg.
+
+Section GroupTheory.
+  Context `{G : Group X}.
+  Implicit Types x y g : G.
+  Lemma mulgA : forall {x y z}, x * (y * z) == (x * y) * z.
+  Proof. now destruct G as [a b c d [[e] f g]]. Qed.
+
+  Lemma mulg1 : forall {x}, x * 1 == x.
+  Proof. now destruct G as [a b c d [e [f] g]]. Qed.
+
+  Lemma mulgV : forall {x}, x * !x == 1.
+  Proof. now destruct G as [a b c d [e f [g]]]. Qed.
+
+  Lemma mul1g : forall {x}, 1 * x == x.
+  Proof. now destruct (@identlg _ G). Qed.
+
+  Lemma mulVg : forall {x}, !x * x == 1.
+  Proof. now destruct (@invlg _ G). Qed.
+
+  Lemma mulgI {g x y} : x * g == y * g -> x == y.
+  Proof.
+    intros H. 
+    now rewrite <-identr, <-(identr y), <-(invr g), 2!assoc, H.
+  Qed.
+
+  Lemma mulIg {g x y} : g * x == g * y -> x == y.
+  Proof.
+    intros H.
+    now rewrite <-identl, <-(identl y), <-(invl g), <-2!assoc, H.
+  Qed.
+  
+  Lemma mulTg {g x y} : g * x == y -> x == !g * y.
+  Proof.
+    intros H. apply (@mulIg g). now rewrite assoc, invr, identl.
+  Qed.
+
+  Lemma mulgT {g x y} : x * g == y -> x == y * !g.
+  Proof.
+    intros H. apply (@mulgI g). now rewrite <-assoc, invl, identr.
+  Qed.
+
+  Lemma invgK {x} : !!x == x.
+  Proof. apply (@mulgI (!x)). now rewrite invl, invr. Qed.
+
+  Lemma eq_invg_sym {x y} : !x == y -> x == !y.
+  Proof. intros H. now rewrite <-H, invgK. Qed.
+ 
+  Lemma invMg {x y} : !(x * y) == !y * !x.
+  Proof.
+    rewrite <-(identr (!x)). apply mulTg, mulTg.
+    now rewrite assoc, invr.
+  Qed.
+
+  Lemma invg1 : !(1 in G) == 1.
+  Proof.
+    rewrite <-(identr (!1)). symmetry; apply mulTg, identl.
+  Qed.
+End GroupTheory.
+
+
+Class IsHomomorph {X Y} {G : Group X} {H : Group Y} (f : Map G H) := {
+  morph : forall {x y : G}, f (x * y) == (f x) * (f y)
+}.
+
+Structure Homomorph {X Y} (G : Group X) (H : Group Y) := {
+  homfun :> Map G H;
+  homprf :> IsHomomorph homfun
+}.
+#[global]
+Existing Instance homprf.
+
+Notation "'hom' 'on' f" := (@Build_Homomorph _ _ _ _ f _)
+  (at level 200, no associativity).
+Notation "'hom' 'by' f " := (hom on (map by f))
+  (at level 200, no associativity).
+Notation " 'hom' x => m " := (hom by fun x => m)
+  (at level 200, x binder, no associativity) : group_scope.
+Notation "G ~~> H" := (@Homomorph _ _ G H)
+  (at level 99, no associativity) : group_scope.
+
+Structure Isomorph {X Y} (G : Group X) (H : Group Y) := {
+  isofun :> Homomorph G H;
+  isoprf :> Bijective isofun
+}.
+#[global]
+Existing Instance isoprf.
+
+Notation "'iso' 'on' f" := (@Build_Isomorph _ _ _ _ f _)
+  (at level 200, no associativity) : group_scope.
+Notation "'iso' x => m " := (iso on hom x => m)
+  (at level 200, x binder, no associativity) : group_scope.
+Notation "G <~> H" := (@Isomorph _ _ G H)
+  (at level 95, no associativity) : group_scope.
+
+Program Definition homcomp {X Y Z} {G1 : Group X} {G2 : Group Y}
+  {G3 : Group Z} (f: G1 ~~> G2) (g: G2 ~~> G3) : G1 ~~> G3
+  := hom on (g o f).
+Next Obligation.
+  split. intros x y. destruct f as [f Homf].
+  destruct g as [g Homg]. simpl.
+  assert (g (f x * f y) == g (f x) * g (f y)).
+  { now rewrite morph. } simpl in H. rewrite <-H.
+  apply (mapprf g). apply morph.
+Defined.
+Notation "g '<o~' f" := (homcomp f g)
+  (at level 60, right associativity) : group_scope.
+
+Program Definition isocomp {X Y Z} {G1 : Group X} {G2 : Group Y}
+{G3 : Group Z} (f: G1 <~> G2) (g: G2 <~> G3) : G1 <~> G3
+  := iso on (g <o~ f).
+Next Obligation.
+  split; split; simpl.
+  - intros x y Heq. simpl in Heq. 
+    now apply (inj g), (inj f) in Heq.
+  - intros y. destruct (surj g y). destruct (surj f x).
+    exists x0. simpl in H. rewrite H. now apply (mapprf g).
+Defined.
+Notation "g '<o>' f" := (isocomp f g)
+  (at level 60, right associativity) : group_scope.
+
+
+Section HomTheory.
+  Context {X Y} {G : Group X} {H : Group Y} {f: Homomorph G H}.
+  Lemma morph1 : f 1 == 1.
+  Proof.
+    apply (mulgI (g := f 1)). now rewrite <-morph, 2!identl.
+  Qed.
+
+  Lemma morphV : forall x, f (!x) == !(f x).
+  Proof.
+    intros x.  rewrite <-(identr (!(f x))).
+    apply mulTg. now rewrite <-morph, invr, morph1.
+  Qed.
+End HomTheory.
+
+
+(* Close Scope group_scope.
+Close Scope setoid_scope. *)
--- theories/setoid.v
+++ theories/setoid.v
@@ -31,20 +31,20 @@ Notation "x == y" := (x == y in _)
 Program Definition PropSetoid := [ ==: iff ].
 Canonical Structure PropSetoid.
 
-Structure Carto (X Y : Setoid) : Type := {
-  cartofun :> X -> Y;
-  cartoprf :> Proper ((==) ==> (==)) cartofun
+Structure Map (X Y : Setoid) : Type := {
+  mapfun :> X -> Y;
+  mapprf :> Proper ((==) ==> (==)) mapfun
 }.
 #[global]
-Existing Instance cartoprf.
+Existing Instance mapprf.
 
-Notation "'carto' 'by' f" := (@Build_Carto _ _ f _)
+Notation "'map' 'by' f" := (@Build_Map _ _ f _)
   (at level 200, no associativity).
-Notation " 'carto' x => m " := (carto by fun x => m)
+Notation "'map' x => m" := (map by fun x => m)
   (at level 200, x binder, no associativity).
 
-Program Definition CartoSetoid (X Y : Setoid) :=
-  [ Carto X Y | ==: ((==) ==> (==))%signature ].
+Program Definition MapSetoid (X Y : Setoid) :=
+  [ Map X Y | ==: ((==) ==> (==))%signature ].
 Next Obligation.
   split.
   - intros f x y Heq. now rewrite Heq. 
@@ -52,40 +52,38 @@ Next Obligation.
   - intros f g h Heq1 Heq2 x y Heq3. 
     now rewrite (Heq1 x y Heq3), <-(Heq2 x y Heq3), Heq3.
 Defined.
-Canonical Structure CartoSetoid.
+Canonical Structure MapSetoid.
 
-Program Definition InducedSetoid {X : Type} {Y : Setoid} (f : X -> Y) :=
+Program Definition InducedSetoid {X} {Y : Setoid} (f : X -> Y) :=
   [ X | ==: fun x1 x2 : X => f x1 == f x2].
 Next Obligation.
   split; intuition. intros x y z Exy Eyz. now rewrite Exy.
 Defined.
 
-Program Definition indsetoid_carto {X : Type} {Y : Setoid} (f : X -> Y)
-  : Carto (InducedSetoid f) Y := carto x => f x.
-Next Obligation.
-  now intros x1 x2 Ex12.
-Defined.  
+Program Definition indsetoid_map {X} {Y : Setoid} (f : X -> Y)
+  : Map (InducedSetoid f) Y := map x => f x.
+Next Obligation. now intros x1 x2 Ex12. Defined.
 
-Inductive sigS `(P : Carto X Prop) : Type :=
+Inductive sigS `(P : Map X Prop) : Type :=
   existS : forall x : X, P x -> sigS P.
 Arguments existS {_} {_} {_} _.
 
-Coercion sval `{P : Carto X Prop} (e : sigS P) :=
+Coercion sval `{P : Map X Prop} (e : sigS P) :=
   match e with (@existS _ _ a b) => a end.
 
-Definition sigSSetoid `(P : Carto X Prop) :=
+Definition sigSSetoid `(P : Map X Prop) :=
   @InducedSetoid (sigS P) X sval.
-Definition incluS `(P : Carto X Prop) : Carto (sigSSetoid P) X := 
-  @indsetoid_carto (sigS P) X sval.
+Definition incluS `(P : Map X Prop) : Map (sigSSetoid P) X := 
+  @indsetoid_map (sigS P) X sval.
 Canonical Structure sigSSetoid.
 
 Structure Ensemble (X : Setoid) : Type := {
-  ensconf :> Carto X Prop;
+  ensconf :> Map X Prop;
 }.
 
 Coercion ens_setoid `(A : Ensemble X) := sigSSetoid (ensconf A).
 
-Notation "[  x  :  A  |  P  ]" := (@Build_Ensemble A (carto x => P))
+Notation "[  x  :  A  |  P  ]" := (@Build_Ensemble A (map x => P))
   (x at level 99).
 Notation "[  x  |  P  ]" := [x : _ | P]
   (x at level 99).
@@ -97,10 +95,16 @@ Notation "A '<=' B" := (@Included _ A B) : setoid_scope.
 
 Lemma included_trans {X} : Transitive (@Included X).
 Proof.
-  intros A B C [LAB] [LBC]. split. intros x. apply (LBC (existS (LAB x))).
+  intros A B C [LAB] [LBC]. split. intros x.
+  apply (LBC (existS (LAB x))).
 Qed.
 #[global]
 Existing Instance included_trans.
+
+Program Definition inclumap {X} {A B : Ensemble X} (H : A <= B)
+  : Map A B := map x => (@existS _ _ (sval x) _).
+Next Obligation. apply H. Defined.
+Next Obligation. intros x y E. now simpl. Defined.
 
 Definition enseq {X} (A B : Ensemble X) := A <= B /\ B <= A.
 Program Definition EnsembleSetoid (X : Setoid) := [ ==: @enseq X].
@@ -115,42 +119,53 @@ Defined.
 Canonical Structure EnsembleSetoid.
 
 Program Definition trivEns (X : Setoid) := [ _ : X | True ].
-Next Obligation.
-  now intros x.
-Defined.
+Next Obligation. now intros x. Defined.
 
-Program Definition Image {X Y} (f : Carto X Y) (A : Ensemble X) :=
+Program Definition Image {X Y} (f : Map X Y) (A : Ensemble X) :=
   [ y | exists a : A, y == f a ].
 Next Obligation.
   intros x y Exy. split; intros [a Exfa]; exists a;
   try rewrite <-Exfa; try rewrite Exy; trivial; reflexivity.
 Defined.
 
-Program Definition Preimage {X Y} (f : Carto X Y) (B : Ensemble Y) :=
+Program Definition Preimage {X Y} (f : Map X Y) (B : Ensemble Y) :=
   [ x | B (f x) ].
-Next Obligation.
-  intros x y Exy. now rewrite Exy.
-Defined.
+Next Obligation. intros x y Exy. now rewrite Exy. Defined.
 
-Structure Map {X Y} (A : Ensemble X) (B : Ensemble Y) := {
-  mapfun :> Carto A B;
+Class Injective `(f : Map A B) := {
+  inj : forall {x y : A}, f x == f y -> x == y
 }.
+Arguments inj {_} {_} _ {_}.
 
-Definition MapSetoid {X Y} (A : Ensemble X) (B : Ensemble Y) :=
-  InducedSetoid (@mapfun _ _ A B).
-
-Class IsInjective {X Y} {A : Ensemble X} {B : Ensemble Y} (f : Map A B) := {
-  inj : forall x y : A, f x == f y -> x == y
+Class Surjective `(f : Map A B) := {
+  surj : forall {y : B}, exists x : A, y == (f x)
 }.
+Arguments surj {_} {_} _ {_}.
 
-Class IsSurjective {X Y} {A : Ensemble X} {B : Ensemble Y} (f : Map A B) := {
-  surj : forall y : B, exists x : A, y == (f x)
+Class Bijective `(f: Map A B) := {
+  bij_inj :> Injective f;
+  bij_surj :> Surjective f
 }.
+#[global]
+Existing Instances bij_inj bij_surj.
 
-Class IsBijective {X Y} {A : Ensemble X} {B : Ensemble Y} (f: Map A B) := {
-  bij_inj :> IsInjective f;
-  bij_surj :> IsSurjective f
-}.
+Program Definition mapcomp {X Y Z} (f: Map X Y) (g: Map Y Z)
+  : Map X Z := map x => g (f x).
+Next Obligation. intros x y Heq. now rewrite Heq. Defined.
+Notation "g 'o' f" := (@mapcomp _ _ _ f g)
+  (at level 60, right associativity) : setoid_scope.
+
+Lemma mapcomp_reduc : forall {X Y Z} {f g: Map Y Z} {h: Map X Y},
+  Surjective h -> f o h == g o h -> f == g.
+Proof.
+  intros X Y Z f g h [Sh] Heq. simpl. intros x y Heq1.
+  rewrite Heq1. destruct (Sh y) as [x0 Heq2].
+  rewrite Heq2. now apply Heq.
+Qed.
+
+Lemma mapcomp_assoc {X Y Z W} {f: Map Z W} {g: Map Y Z} {h: Map X Y} :
+  (f o g) o h == f o g o h.
+Proof. intros. simpl. intros x y Heq. now rewrite Heq. Qed.
 
 Close Scope setoid_scope.
 
