dccd509eea00b94af4a1b4736afead567369bb20
Lomega <atibon3@gmail.com>
2023-01-28 20:18:17 +0900

    OppCat

--- theories/category.v
+++ theories/category.v
@@ -12,8 +12,8 @@ Declare Scope cat_scope.
 Open Scope setoid_scope.
 Open Scope cat_scope.
 
-Check transitive.
-Check transitivity.
+Notation "p .1" := (fst p) (at level 5, left associativity, format "p .1").
+Notation "p .2" := (snd p) (at level 5, left associativity, format "p .2").
 
 Lemma trans {X : Setoid} {a b c : X} : a == b -> b == c -> a == c.
 Proof. intros H H0. now rewrite H. Defined.
@@ -23,18 +23,15 @@ Proof.
   intros E E0 E1. now rewrite <-E0, <-E1.
 Defined.
 
-Program Definition ligature {X Y : Setoid} (Cod Dom : Map X Y) :=
+Program Definition composables {X Y : Setoid} (Cod Dom : Map X Y) :=
   [ p | Cod (fst p) == Dom (snd p) ].
 Next Obligation.
   intros [x y] [x0 y0] [E E0]. split; intros H; simpl in *;
   now rewrite2 E; rewrite2 E0.
 Defined.
 
-Notation "p .1" := (fst p) (at level 5, left associativity, format "p .1").
-Notation "p .2" := (snd p) (at level 5, left associativity, format "p .2").
-
 Class IsCategory (obj hom : Setoid) (dom cod : Map hom obj)
-  (comp : Map (ligature cod dom) hom) (id : obj -> hom) :=
+  (comp : Map (composables cod dom) hom) (id : obj -> hom) :=
 {
   comp_dom : forall p, dom (comp p) == dom ($p).1;
   comp_cod : forall p, cod (comp p) == cod ($p).2;
@@ -51,7 +48,7 @@ Structure Category := {
   cathom : Setoid;
   dom : Map cathom catobj;
   cod : Map cathom catobj;
-  catcomp : Map (ligature cod dom) cathom;
+  catcomp : Map (composables cod dom) cathom;
   catid : Map catobj cathom;
 
   catprf :> IsCategory catcomp catid
@@ -61,8 +58,8 @@ Structure Category := {
 Arguments dom {_}.
 Arguments cod {_}.
 
-Notation "[ 'o:' comp , '1:' id ]"
-  := (@Build_Category _ _ _ _ comp id _)
+Notation "[ 'dom:' dom , 'cod:' cod , 'o:' comp , '1:' id ]"
+  := (@Build_Category _ _ dom cod comp id _)
   (at level 0, comp, id at level 99) : cat_scope.
 Notation "1_ A" := (@catid _ A)
   (at level 0, A at level 99, no associativity,
@@ -102,13 +99,18 @@ Next Obligation.
   simpeq_all. apply map_equal. split; now simpl.
 Defined.
 
+Lemma CDeq_pair {X : Category} {f g : cathom X}
+  : cod f == dom g -> cod (f, g).1 == dom (f, g).2.
+Proof. intuition. Defined.
 
-
-
-Notation "g 'o' f" := (@catcomp _ _ _ _ f g)
+Notation "g o[ 'by' H ] f" := (@catcomp _ $[(f, g), H])
+  (at level 60, right associativity, format "g  o[ 'by'  H ]  f")
+  : cat_scope.
+Notation "g 'o' f" := (@catcompDC _ _ _ _ f g)
   (at level 60, right associativity) : cat_scope.
 
-Class IsInverse (C : Category) (A B : C) (f : A ~> B)
+
+(* Class IsInverse (C : Category) (A B : C) (f : A ~> B)
    (g : B ~> A) := {
   invcomp1 : g o f == 1_A;
   invcomp2 : f o g == 1_B
@@ -119,21 +121,63 @@ Structure Isomorph (C : Category) (A B : C) := {
   invhom : B ~> A;
   isoprf :> IsInverse orthohom invhom
 }.
-#[global] Existing Instance isoprf.
+#[global] Existing Instance isoprf. *)
+
+Lemma compeq {X : Category} {f g : cathom X} H H0 :
+  g o[by H] f == g o[by H0] f.
+Proof. now apply map_equal. Qed.
+
+Lemma compceq {X : Category} {f g h : cathom X} :
+  forall H H0 H1 H2,
+  h o[by H0] g o[by H] f == h o[by H2] g o[by H1] f.
+Proof. 
+  intros. apply map_equal. split; try now simpl. apply compeq.
+Qed.
+
+Lemma ccompeq {X : Category} {f g h : cathom X} :
+  forall H H0 H1 H2,
+  (h o[by H] g) o[by H0] f == (h o[by H1] g) o[by H2] f.
+Proof. 
+  intros. apply map_equal. split; try now simpl. apply compeq.
+Qed.
 
 Program Definition OppCat (X : Category) :=
-  [ _ | ~>: fun A B => X B A,
-        o: fun A B C => dmap f g => f o g,
-        1: fun A => 1_A ].
+  [ dom: @cod X, cod: @dom X,
+     o: map p => ($p).1 o[by _] ($p).2, 1: catid X ].
 Next Obligation.
-  intros f f0 H g g0 H0. now rewrite H, H0.
+  destruct p as [[f g] H]. simpl in *. now symmetry.
+Defined.
+Next Obligation.
+  intros [[f g] H] [[f0 g0] H0] [E E0]. simpeq_all.
+  apply map_equal. split; now simpl.
 Defined.
 Next Obligation.
   split; intuition; simpl.
-  - now srewrite compcA.
-  - now srewrite comp1c.
-  - now srewrite compc1.
+  - apply comp_cod.
+  - apply comp_dom.
+  - pose ( (compcA(IsCategory:=X))). simpl in e.
+    destruct p as [[h g] Hp]. destruct p0 as [[g0 f] Hp0].
+    simpl in *. pose (g, h) as p.
+    assert (ensconf (composables cod dom) p) as H2 by now simpl.
+    pose (f, g) as p0.
+    assert (ensconf (composables cod dom) p0) as H3.
+    { simpl. rewrite <-Hp0. now apply map_equal. }
+    pose (@e $[p0, H3] $[p, H2]).
+    unfold p, p0 in e0. simpl in e0.
+    assert (g == g) by reflexivity. pose (e0 H4).
+    assert (cod f == dom (h o[by H2] g)).
+    { now rewrite comp_dom. }
+    assert (cod (g o[by H3] f) == dom h).
+    { now rewrite comp_cod. }
+    pose (e1 H5 H6). rewrite ccompeq. rewrite e2.
+    apply map_equal. split; try now simpl.
+    apply map_equal. split; now simpl.
+  - apply id_cod.
+  - apply id_dom.
+  - apply comp1c.
+  - apply compc1.
 Defined.
+
 Notation "C ^op" := (@OppCat C)
   (at level 40, left associativity, format "C ^op") : cat_scope.
 
